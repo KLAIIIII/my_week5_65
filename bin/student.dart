29276c2f5a39c08a9cb8c0fd6e4eb1cf4990f370
KLAIIIII <guyyg03546@gmail.com>
2022-06-17 16:29:52 +0700

    update

--- bin/student.dart
+++ bin/student.dart
@@ -1,17 +1,24 @@
 void main(){
 //  new instance of student object
-  Student std1 = Student();
+  Student std1 = Student(stdName: "Mark", stdAge:46,stdId: 101);
   print(std1);
 //  set the properties
-  std1.stdName = "Mark";
-  std1.stdAge = 49;
-  std1.stdId = 101;
+//   std1.stdName = "Mark";
+//   std1.stdAge = 49;
+//   std1.stdId = 101;
 //  access the properties
   print('${std1.stdName} ${std1.stdAge} ${std1.stdId}');
 }
 
 class Student{
-  String? stdName;
-  int? stdAge;
-  int? stdId;
+  String stdName;
+  int stdAge;
+  int stdId = 101;
+  //Constructor
+  //1.setup default constructor with argument
+  // Student(this.stdName,this.stdAge,this.stdId);
+  //2.setup default constructor with optional argument
+  // Student([this.stdName,this.stdAge,this.stdId]);
+  //3.setup default constructor with name argument
+  Student({required this.stdName,required this.stdAge,required this.stdId});
 }
